fdc510c1cd809429170418ee4f78523868d40838
alex-seville <alexander.southern@gmail.com>
2012-08-13 08:00:52 -0400

    Changed parameters, might need to redo, and test not working, but I need to commit

--- example/example.js
+++ example/example.js
@@ -1,6 +1,6 @@
 var contentMapper = require("../lib/contentMapper.js");
 
-contentMapper("http://www.reddit.com",{
+contentMapper({ inputfile:"http://www.reddit.com",
 		mappingFn:function($){
 			return {headline: $('.content .spacer:last .entry:first a:first').text()};
 		},
